6da3ca76e8340e7a5a970cef56ba13b75e7ac857
AnkurSh0403 <ankursharma.as0403@gmail.com>
2025-09-23 23:39:05 +0530

    longest_repeating_character_replacement optimised code

--- sliding_window_and_two_pointer/longest_repeated_character_replacement.cpp
+++ sliding_window_and_two_pointer/longest_repeated_character_replacement.cpp
@@ -1,4 +1,6 @@
 //S=AAABBCCD     k=2
+
+//Brute Force  --> O(nlogn) logn for accessing in map
 #include<iostream>
 #include<map>
 using namespace std;
@@ -23,6 +25,38 @@ int main(){
             start=j;
         }
         
+    }
+    cout<<maxlen<<s.substr(start,maxlen);
+    
+return 0;
+}
+
+//more optimised  Access time O(1)
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+int main(){
+    int k;
+    string s;
+    vector<int>freq(26,0);
+    cin>>k>>s;
+    int maxlen=0;
+    int maxf=0;
+    int j=0;
+    int start=0;
+    for(int i=0;i<s.size();i++){
+        freq[s[i]-'A']++;
+        maxf=max(maxf,freq[s[i]-'A']);
+        while((i-j+1)-maxf>k){
+            freq[s[j]-'A']--;
+            j++;
+        }
+        if(i-j+1>maxlen){
+            maxlen=i-j+1;
+            start=j;
+        }
     }
     cout<<maxlen<<s.substr(start,maxlen);
     
